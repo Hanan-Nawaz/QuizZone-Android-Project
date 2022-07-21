c8c9947a3ca6aa8b653ef88b83355ea80a29df83
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-21 18:45:03 +0500

    Fragment for Login with Added

---
+++ app/src/main/java/com/example/quizzone/Fragments/login_with.java
@@ -0,0 +1,26 @@
+package com.example.quizzone.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizzone.R;
+
+public class login_with extends Fragment {
+
+    public login_with() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_loginwith, container, false);
+        return view;
+    }
+}
