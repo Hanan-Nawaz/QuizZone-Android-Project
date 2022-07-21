398ee200c5f352f9526aca9ef3faaab46f52bc0c
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-21 22:38:41 +0500

    Fragment for Implemented and Working Fine (Design Only)

--- app/src/main/java/com/example/quizzone/MainActivity.java
+++ app/src/main/java/com/example/quizzone/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.quizzone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
+
+import com.example.quizzone.Fragments.login_with;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        if(savedInstanceState == null){
+            Bundle bundle = new Bundle();
+            bundle.putInt("15", 0);
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_viewer , login_with.class, bundle).commit();
+
+        }
+
     }
 }
