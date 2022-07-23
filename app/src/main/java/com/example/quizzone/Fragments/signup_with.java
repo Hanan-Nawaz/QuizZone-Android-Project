fdc5f679450fcf647f29bf762f2f9c61f0f82961
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-23 13:36:41 +0500

    Fragments Backend Implemented

--- app/src/main/java/com/example/quizzone/Fragments/signup_with.java
+++ app/src/main/java/com/example/quizzone/Fragments/signup_with.java
@@ -2,7 +2,6 @@ package com.example.quizzone.Fragments;
 
 import android.os.Bundle;
 
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,38 +11,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-
 import com.example.quizzone.R;
 
-public class login_with extends Fragment {
+public class signup_with extends Fragment {
+
 
     Button btn;
 
-    public login_with() {
+    public signup_with() {
         // Required empty public constructor
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_loginwith, container, false);
+        View view = inflater.inflate(R.layout.fragment_signup_with, container, false);
 
         btn = view.findViewById(R.id.btn_auth);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signup_with signupWith = new signup_with();
+                login_with loginWith = new login_with();
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.hide(login_with.this);
-                fragmentTransaction.add(R.id.fragment_viewer, signupWith);
+                fragmentTransaction.hide(signup_with.this);
+                fragmentTransaction.add(R.id.fragment_viewer, loginWith);
                 fragmentTransaction.commit();
             }
         });
 
         return view;
-
-
     }
 }
