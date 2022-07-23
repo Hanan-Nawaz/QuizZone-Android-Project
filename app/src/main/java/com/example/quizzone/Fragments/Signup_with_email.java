e5063a4f9b38effa40a15044daf7f4b341ceec78
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-23 15:06:44 +0500

    Back button Integrated in SignUp with Email Fragment

--- app/src/main/java/com/example/quizzone/Fragments/Signup_with_email.java
+++ app/src/main/java/com/example/quizzone/Fragments/Signup_with_email.java
@@ -1,16 +1,23 @@
 package com.example.quizzone.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.quizzone.Activities.SignInSignUp.AuthActivity;
 import com.example.quizzone.R;
 
 public class Signup_with_email extends Fragment {
+
+    Button back;
 
     public Signup_with_email() {
         // Required empty public constructor
@@ -21,6 +28,17 @@ public class Signup_with_email extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_signup_with_email, container, false);
+
+        back = view.findViewById(R.id.Back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), AuthActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
         return view;
     }
 }
