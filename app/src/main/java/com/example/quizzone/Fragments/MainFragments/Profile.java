923f37791f1f410e946d4bed6e739372027cdaaf
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-24 20:35:44 +0500

    Profile Page Added

---
+++ app/src/main/java/com/example/quizzone/Fragments/MainFragments/Profile.java
@@ -0,0 +1,29 @@
+package com.example.quizzone.Fragments.MainFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizzone.R;
+
+public class Profile extends Fragment {
+
+    public Profile() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+        return view;
+    }
+}
